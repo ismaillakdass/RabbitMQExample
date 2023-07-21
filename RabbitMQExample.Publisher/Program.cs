b9070fa73b0ca84678c6a6a0bfc5d60f2cb88326
ismaillakdass <ismaillakdass@gmail.com>
2023-07-22 00:07:05 +0300

    Add project files.

---
+++ RabbitMQExample.Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿//Publisher
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQExample.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            // Rabbitmq services inde oluşturduğumuz url
+            factory.Uri = new Uri("amqps://taktjuar:y1W2ZxWkT0GZ_iXsjTQaVw9HndVcRyE5@shark.rmq.cloudamqp.com/taktjuar");
+
+            using (var connection = factory.CreateConnection())
+            {
+                // Kanal oluşturuluyor
+                var channel = connection.CreateModel();
+
+                // Kuyruk oluşturuluyor
+                // 1.Param= Kuyruk adı, 2. Param= kuyruk fiziksel bellekte oluşup uluşturmama, 3. Param = kuyruğa sadece burdan ulaşıp ulaşmamak yani aslında public olup olmaması, 4. Param = kuyruk da bağlı herhangi bir subscribe kalmadığında kuyruğun silinip silinmemesi 
+                channel.QueueDeclare("hello-queue", true,false,false);
+
+                string message = "hello world";
+
+                var messagebody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(string.Empty, "hello-queue",null, messagebody);
+                
+                Console.WriteLine("Mesaj Gönderildi");
+                
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ RabbitMQExample.Subscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿//Subscriber
+
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQExample.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            // Rabbitmq services inde oluşturduğumuz url
+            factory.Uri = new Uri("amqps://taktjuar:y1W2ZxWkT0GZ_iXsjTQaVw9HndVcRyE5@shark.rmq.cloudamqp.com/taktjuar");
+
+            using (var connection = factory.CreateConnection())
+            {
+                // Kanal oluşturuluyor
+                var channel = connection.CreateModel();
+
+                // Subscriber tarafında bu isimde kuyruk varsa bu satırın bir zararı yok eğer publisher tarafında böyle bir kuyruk yoksa oluşturur varsa olanı kullanır. Publisher tarafında ki parametrelerle aynı olması önemli.
+                //channel.QueueDeclare("hello-queue", true, false, false);
+
+                var subscriber = new EventingBasicConsumer(channel);
+
+                //Kanal üzerindeki kuyruğa ulaşıyoruz 1. param bize true ile kuyruktan mesaj alındığında mesaj otomatik silinsini ifade eder, false da ise mesajı aldığımızda mesaj kalsın silmek için ben sana dönüş yapacağım anlamına gelir ve silme işlemi bizim kontrolümüz altında olur. , 2. paramda subscribe diğer adıyla consumer i veriyoruz.
+                channel.BasicConsume("hello-queue",true,subscriber);
+
+                subscriber.Received += (object sender, BasicDeliverEventArgs e) =>
+                {
+                    var message= Encoding.UTF8.GetString(e.Body.ToArray());
+                    Console.WriteLine("Gelen Mesaj : " + message);
+                };
+
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
